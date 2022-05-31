6ce3b6a9f7ff5623dd1bc563b0ce6b98f291071f
sin42 <sin42@DESKTOP-MQ9KKGC>
2022-05-31 12:13:19 +0530

    Updated

---
+++ src/test/java/PracticeTest/SamplePracticeTest.java
@@ -0,0 +1,16 @@
+package PracticeTest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SamplePracticeTest 
+{
+
+			@Test
+			public void testName() throws Exception 
+			{
+				String browser = System.getProperty("browser");
+				Reporter.log("Byyeee",true);
+				Reporter.log(browser,true);
+			}
+}
